7a0511f95877a0dc75dbade57c92d3c84fa837a2
Vinícius Romualdo <viniromualdo082@gmail.com>
2023-10-31 15:36:11 -0300

    atividade pratica 5 resolvida

--- atividade_pratica_5.lisp
+++ atividade_pratica_5.lisp
@@ -4,7 +4,14 @@
 ( defun ordena(fun L)
     ( cond
         ( (null L) nil)
-        ( T (cons (funcall fun L) (ordena fun (remove (funcall fun L) L) )) )
+        ( T (cons (funcall fun L) (ordena fun (remover (funcall fun L) L) )) )
+    )
+)
+
+( defun remover(e L)
+    ( cond
+        ( (equal e (car L)) (cdr L) )
+        ( T (cons (car L) (remover e (cdr L)) ) )
     )
 )
 
@@ -22,4 +29,20 @@
         ( (> (car L) (maior (cdr L)) ) (car L))
         ( T (maior (cdr L)) )
     )    
+)
+
+// uso de lambda na chamada de ordena
+( ordena (function (lambda (L)     
+    ( cond
+        ( (null (cdr L)) (car L) )
+        ( (< (car L) (menor (cdr L)) ) (car L))
+        ( T (menor (cdr L)) )
+    )   )) '(1 90 14 89 2 33)
+)
+
+// uso de MAPCAR com lambda
+(mapcar (lambda (x y)
+          (min x Y))
+        '(1 2 3 20 10)
+        '(20 32 12 9 8)
 )
